3c2775a4f39b2c8631b84042c6bccca7bb48aa76
Nguyễn Cao Huy Hoàng <61876488+antoinenguyen-09@users.noreply.github.com>
2021-05-18 22:36:44 +0700

    Update client.py

--- client.py
+++ client.py
@@ -1,1 +1,10 @@
+import socket
 
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect(("tranfer.sh",80))
+    s.sendall(b"GET / HTTP/1.1\r\nHost: transfer.sh\r\nAccept: text/html\r\nConnection: keep-alive\r\n\r\n")
+    while 1:
+        data = s.recv(1024)
+        if not data:
+            break
+        print(data.decode())
