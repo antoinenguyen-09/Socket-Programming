26d1928ea828dec1dc299d879a03a42a2ed79f77
Nguyễn Cao Huy Hoàng <61876488+antoinenguyen-09@users.noreply.github.com>
2021-05-18 23:55:45 +0700

    Update client.py

--- client.py
+++ client.py
@@ -4,7 +4,7 @@ import socket
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 
-    s.connect(("144.76.136.153",80)) # using socket.gethostbyname('transfer.sh') to gain IP add
+    s.connect(("144.76.136.153",80)) # using socket.gethostbyname('transfer.sh') to gain IP address of host
     s.sendall(b"GET / HTTP/1.1\r\nHost: transfer.sh\r\nAccept: text/html\r\nConnection: keep-alive\r\n\r\n")
 
     while 1:
