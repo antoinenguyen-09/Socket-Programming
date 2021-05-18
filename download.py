6ddde7849b4614dfa2d13622e8d2e1ed775bf0f0
Nguyễn Cao Huy Hoàng <61876488+antoinenguyen-09@users.noreply.github.com>
2021-05-19 00:26:28 +0700

    Create download.py

---
+++ download.py
@@ -0,0 +1,21 @@
+import socket
+import select
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(('httpbin.org', 80))
+s.sendall(b'GET /pAsmS/HoangNCH_challege4.md HTTP/1.1\r\nHOST: transfer.sh\r\n\r\n')
+
+reply = b''
+
+while select.select([s], [], [], 3)[0]:
+    data = s.recv(2048)
+    if not data: break
+    reply += data
+
+headers =  reply.split(b'\r\n\r\n')[0]
+image = reply[len(headers)+4:]
+
+# save image
+f = open('HoangNCH_challege4.md', 'wb')
+f.write(image)
+f.close()
